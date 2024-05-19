c6280c4c283c345ecb22fb557ae0ce9e7e89220e
amoussa <amoussa.rayan77130@hotmail.fr>
2024-05-19 23:16:40 +0200

    ajout dependence JUnit

---
+++ prototype/xmart-select-client/src/test/java/SelectClientTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import edu.ezip.ing1.pds.client.MainSelectClient;
+
+
+public class SelectClientTest {
+
+    @Test
+    public void selectPlayerTest(){
+
+        MainSelectClient.selectAllPlayers();
+
+    }
+
+
+
+
+
+    
+}
+
